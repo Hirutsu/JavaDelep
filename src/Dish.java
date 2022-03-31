effd8cf478ec013022f59dc6691f84ae26cf8e9a
Levan Burchuladze <official_hirutsu@mail.ru>
2022-03-31 21:41:06 +0400

    task 3

--- src/Dish.java
+++ src/Dish.java
@@ -1,2 +1,102 @@
-package PACKAGE_NAME;public class Dish {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Dish {
+    public Product[] products;
+
+    public  Dish(){
+        this.products = CreateDish();
+    }
+
+    public void FindProductBW(){
+        Scanner in = new Scanner(System.in);
+        int kalS, kalE, weightS, weightE;
+        try {
+            boolean flag = false;
+            System.out.print("Input kalS: ");
+            kalS = in.nextInt();
+            System.out.print("Input kalE: ");
+            kalE = in.nextInt();
+            System.out.print("Input weightS: ");
+            weightS = in.nextInt();
+            System.out.print("Input weightE: ");
+            weightE = in.nextInt();
+            for (int i = 0; i < this.products.length; i++){
+                if(kalS < this.products[i].GetKal() && this.products[i].GetKal() < kalE){
+                    if(weightS < this.products[i].GetWeight() && this.products[i].GetWeight() < weightE){
+                        System.out.println(this.products[i].Display());
+                        flag = true;
+                    }
+                }
+            }
+            if(flag == false){
+                System.out.println("Nothing with parameters");
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println("Please, input correct values");
+        }
+    }
+
+    public int CalculateKal(){
+        int sum = 0;
+        try{
+            for (int i = 0; i < this.products.length; i++){
+                sum += this.products[i].GetKal();
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return sum;
+        }
+        return sum;
+    }
+
+    private Product[] CreateDish(){
+        Scanner in = new Scanner(System.in);
+
+        int n = 1;
+        Product[] dish = new Product[n];
+        try{
+            for (int i = 0; i < dish.length; i++){
+                String name;
+                int kal;
+                int weight;
+
+                System.out.print("Input type product (Vegetable,Meat,Fruit,Fish):");
+                String type = in.next();
+
+                System.out.print("Input name product :");
+                name = in.next();
+                System.out.print("Input kal in product :");
+                kal = in.nextInt();
+                System.out.print("Input weight in product :");
+                weight = in.nextInt();
+
+                switch (type){
+                    case "Vegetable":
+                        dish[i] = new Vegetable(name,kal,weight);
+                        break;
+                    case "Meat":
+                        dish[i] = new Meat(name,kal,weight);
+                        break;
+                    case "Fruit":
+                        dish[i] = new Fruit(name,kal,weight);
+                        break;
+                    case "Fish":
+                        dish[i] = new FIsh(name,kal,weight);
+                        break;
+                    default:
+                        System.out.println("A u stupid? WrongData");
+                        return null;
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+        return dish;
+    }
 }
--- src/MyApplicationTwo.java
+++ src/MyApplicationTwo.java
@@ -14,96 +14,25 @@ public class MyApplicationTwo {
 
     private void startApplication() {
         int n = 3;
-        Product[] first = CreateDish();
+        Dish dish = new Dish();
 
-        if(first == null){
-            System.out.println("Invalid product");
+        if(dish.products == null){
+            System.out.println("Invalid product, please repeat");
+            return;
         }
         else{
-            for (int i = 0; i < first.length; i++){
-                System.out.println(first[i].Display());
+            for (int i = 0; i < dish.products.length; i++){
+                System.out.println(dish.products[i].Display());
             }
-            System.out.println("All kal in product: " + CalculateKal(first));
+            System.out.println("All kal in product: " + dish.CalculateKal());
         }
 
-        Arrays.sort(first);
+        Arrays.sort(dish.products);
 
-        for (int i = 0; i < first.length; i++){
-            System.out.println(first[i].Display());
+        for (int i = 0; i < dish.products.length; i++){
+            System.out.println(dish.products[i].Display());
         }
 
-        FindProductBW(first);
-
-    }
-
-    private Product[] CreateDish(){
-        Scanner in = new Scanner(System.in);
-
-        int n = 3;
-        Product[] dish = new Product[n];
-
-        for (int i = 0; i < dish.length; i++){
-            String name;
-            int kal;
-            int weight;
-
-            System.out.print("Input type product (Vegetable,Meat,Fruit,Fish):");
-            String type = in.next();
-
-            System.out.print("Input name product :");
-            name = in.next();
-            System.out.print("Input kal in product :");
-            kal = in.nextInt();
-            System.out.print("Input weight in product :");
-            weight = in.nextInt();
-
-            switch (type){
-                case "Vegetable":
-                    dish[i] = new Vegetable(name,kal,weight);
-                    break;
-                case "Meat":
-                    dish[i] = new Meat(name,kal,weight);
-                    break;
-                case "Fruit":
-                    dish[i] = new Fruit(name,kal,weight);
-                    break;
-                case "Fish":
-                    dish[i] = new FIsh(name,kal,weight);
-                    break;
-                default:
-                    System.out.println("A u stupid? WrongData");
-                    return null;
-            }
-        }
-        return dish;
-    }
-
-    private void FindProductBW(Product[] product){
-        Scanner in = new Scanner(System.in);
-        int kalS, kalE, weightS, weightE;
-        System.out.print("Input kalS: ");
-        kalS = in.nextInt();
-        System.out.print("Input kalE: ");
-        kalE = in.nextInt();
-        System.out.print("Input weightS: ");
-        weightS = in.nextInt();
-        System.out.print("Input weightE: ");
-        weightE = in.nextInt();
-
-        for (int i = 0; i < product.length; i++){
-            if(kalS < product[i].GetKal() && product[i].GetKal() < kalE){
-                if(weightS < product[i].GetWeight() && product[i].GetWeight() < weightE){
-                    System.out.println(product[i].Display());
-                }
-            }
-        }
-    }
-
-    private int CalculateKal(Product[] product){
-        int sum = 0;
-        for (int i = 0; i < product.length; i++){
-            sum += product[i].GetKal();
-        }
-        return sum;
+        dish.FindProductBW();
     }
 }
